2a9cb4c2b5b94f6d188a203d5653f6d4bed9dffe
Tavo Cruz <madecruz2@gmail.com>
2023-09-11 09:58:40 -0700

    create user class interface user.h

---
+++ User.h
@@ -0,0 +1,22 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+#include <ostream>
+// #include "List.h"
+// #include "BST.h"
+using namespace std;
+
+class user{
+    private:
+        string firstName;
+        string lastName;
+        string userName;
+        string passWord;
+        string city;
+        string company;
+        int userId;
+
+}
+
+#endif /* USER_H */
